05a6ab9a29428c988c443bf9731625655da972fd
Amisha Khichariya <amishaak38@gmail.com>
2025-09-24 14:21:44 +0530

    model training

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.datascience.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SCHEMA_FILE_PATH
 
 print("CONFIG:", CONFIG_FILE_PATH, CONFIG_FILE_PATH.exists())
@@ -35,11 +35,21 @@ STAGE_NAME="data transformation stage"
 
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj=DataTransformationTrainingPipeline() 
-        obj.initiate_data_transformation()
+        data_transformation=DataTransformationTrainingPipeline() 
+        data_transformation.initiate_data_transformation()
         logger.info(f">>>>>>> stage {STAGE_NAME} empleted<<<<<<")
 except Exception as e:
         logger.exception(e)
         raise e  
 
+STAGE_NAME="model trainer stage"
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_trainer=ModelTrainerTrainingPipeline() 
+        model_trainer.initiate_model_trainer()
+        logger.info(f">>>>>>> stage {STAGE_NAME} empleted<<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e  
 
